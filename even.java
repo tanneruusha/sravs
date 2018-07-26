b13486d9377a90a3bfa5dfaf7f0e5879e35852e9
tanneruusha <41734192+tanneruusha@users.noreply.github.com>
2018-07-26 20:38:20 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number");
+		int number=sc.nextInt();
+		if(number%2==0)
+		{
+			System.out.println("number is even");
+		}
+		else
+		{
+			System.out.println("number is odd");
+		}
+		}
+		}
+	
